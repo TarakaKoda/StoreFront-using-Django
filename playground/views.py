3ac78d99fbd16e54585f6f70eecb55fab86f0dba
TarakaKoda <taraka.koda@gmail.com>
2023-08-16 15:12:15 +0530

    added tested and implemented caching using redis for playground api

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from django.core.mail import send_mail, mail_admins, EmailMessage, BadHeaderError
-from templated_mail.mail import BaseEmailMessage
-from .tasks import notify_customers
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework.views import APIView
+import requests
 
+class PlaygroundView(APIView):
+    @method_decorator(cache_page(5 * 60))
+    def get(self, request):
+        response = requests.get('https://httpbin.org/delay/2')
+        data = response.json()
+        return render(request, 'hello.html', {'name': 'Taraka'})
 
-def say_hello(request):
-    try:
-        notify_customers.delay('hello')
-    except BadHeaderError:
-        pass
-    return render(request, 'hello.html', {'name': 'Tarak'})
--- storefront/settings.py
+++ storefront/settings.py
@@ -193,3 +193,13 @@ CELERY_BEAT_SCHEDULE = {
 }
 
 CELERY_WORKER_PREFETCH_MULTIPLIER = 1
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
